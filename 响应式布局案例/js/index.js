469d192f3aefb8cda539da2f88926280710eb9d7
liugr <979086560@qq.com>
2022-07-29 17:51:44 +0800

    2022-7-29

--- 响应式布局案例/js/index.js
+++ 响应式布局案例/js/index.js
@@ -1,17 +1,51 @@
+window.addEventListener('DOMContentLoaded',function(){
+    
+    
+    //  解决应小窗口打开  导航栏  未关闭   切换到  大窗口的  显示问题
+
+
+
+
+    let tabtn =document.querySelectorAll('.recommend-items');
+    let tablist = [...tabtn[0].children,...tabtn[1].children];
+    let tabitem = document.querySelector('.recommend-count').children;
+
+    // tab 栏  案例切换
+    for(let i = 0 ; i <= tablist.length - 1 ; i++ ) {
+        let index = i;
+        index = index >= tabitem.length ? index-tabitem.length : index;
+        tablist[i].addEventListener('click',function(){
+            for(let i = 0 ; i <= tablist.length - 1 ; i++ ){
+                tablist[i].classList.remove('current');
+            }
+            for(let i = 0 ; i <= tabitem.length - 1 ; i++) {
+                tabitem[i].classList.remove('current');
+            }
+            this.classList.add('current');
+            tabitem[index].classList.add('current');
+        })
+    }
+    
+})
 let btn1 = document.querySelector("#small-list");
-        let btnIcon = document.querySelectorAll('.small-icon');
-        let btn2 =document.querySelector('.icon-hidden');
-         function changeOff(){
-            btn1.style.display = 'block';
-            btnIcon.forEach(function(ele){
-                ele.style.display = 'none';
-                btn2.style.display = 'block';
-            })
-         }
-         function changeOn(){
-            btn1.style.display = 'none';
-            btnIcon.forEach(function(ele){
-                ele.style.display = 'block';
-                btn2.style.display = 'none';
-            })
-         }
+    let btnIcon = document.querySelectorAll('.small-icon');
+    let btn2 =document.querySelector('.icon-hidden');
+    let page = document.querySelector('article');
+    // 小窗口下 导航栏的  打开按钮
+     function changeOff(){
+        btn1.style.display = 'block';
+        btnIcon.forEach(function(ele){
+            ele.style.display = 'none';
+        })
+        btn2.style.display = 'block';
+        page.style.display = 'none';
+     }
+    //  小窗口下 导航栏 关闭按钮
+     function changeOn(){
+        btn1.style.display = 'none';
+        btnIcon.forEach(function(ele){
+            ele.style.display = 'block';
+        })
+        btn2.style.display = 'none';
+        page.style.display = 'block';
+     }
